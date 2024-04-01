c0e59da7fa57cc18b15a240480c384646eef0999
aasocial <143957935+aasocial@users.noreply.github.com>
2024-04-02 01:58:21 +0900

    Add files via upload

---
+++ object_detection_raspberry_pi/detect.py
@@ -0,0 +1,203 @@
+
+"""
+Naming Convention:
+Battery1 = 25% charged
+Battery2 = 50% charged
+Battery3 = 75% charged
+Battery4 = 100% charged
+"""
+
+#Libraries for Object Detection
+import argparse
+import sys
+import time
+
+import cv2
+import mediapipe as mp
+
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+from utils import visualize
+
+# Libraries for Servo Motor Control
+import RPi.GPIO as GPIO
+import time
+
+# Global variables to calculate FPS
+COUNTER, FPS = 0, 0
+START_TIME = time.time()
+
+# Global variable for detecting battery percentage
+battery_counter = 0
+"""
+Naming Convention for battery_counter:
+1 -- 25%
+2 -- 50%
+3 -- 75%
+4 -- 100%
+"""
+GPIO.setmode(GPIO.BOARD)
+    # Set pin 11  as an output, and set servo1 as pin 11 as PWM
+    # servo can be set to I/O pins: ,11,12,13,15,16,18,22
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) # 11 is the pin number, 50 = 50Hz pulse
+servo1.start(0) #start PWM running, but with value of 0
+    #time.sleep() delay in seconds
+angle1 = 2+(0/18)
+angle2 = 2+(85/18)
+
+def motorcontrol() -> None:
+    global battery_counter
+    if battery_counter == '5':
+        servo1.ChangeDutyCycle(angle2)
+    if battery_counter != "5":
+        servo1.ChangeDutyCycle(angle1)
+    battery_counter = 0
+
+
+def run(model: str, max_results: int, score_threshold: float,
+        camera_id: int, width: int, height: int) -> None:
+  """Continuously run inference on images acquired from the camera.
+
+  Args:
+    model: Name of the TFLite object detection model.
+    max_results: Max number of detection results.
+    score_threshold: The score threshold of detection results.
+    camera_id: The camera id to be passed to OpenCV.
+    width: The width of the frame captured from the camera.
+    height: The height of the frame captured from the camera.
+  """
+
+  # Start capturing video input from the camera
+  cap = cv2.VideoCapture(camera_id)
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+  # Visualization parameters
+  row_size = 50  # pixels
+  left_margin = 24  # pixels
+  text_color = (0, 0, 0)  # black
+  font_size = 1
+  font_thickness = 1
+  fps_avg_frame_count = 10
+
+  detection_frame = None
+  detection_result_list = []
+
+  def save_result(result: vision.ObjectDetectorResult, unused_output_image: mp.Image, timestamp_ms: int):
+      global FPS, COUNTER, START_TIME, battery_counter
+
+      # Calculate the FPS
+      if COUNTER % fps_avg_frame_count == 0:
+          FPS = fps_avg_frame_count / (time.time() - START_TIME)
+          START_TIME = time.time()
+
+      detection_result_list.append(result)
+      COUNTER += 1
+
+  # Initialize the object detection model
+  base_options = python.BaseOptions(model_asset_path=model)
+  options = vision.ObjectDetectorOptions(base_options=base_options,
+                                         running_mode=vision.RunningMode.LIVE_STREAM,
+                                         max_results=max_results, score_threshold=score_threshold,
+                                         result_callback=save_result)
+  detector = vision.ObjectDetector.create_from_options(options)
+
+
+  # Continuously capture images from the camera and run inference
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      sys.exit(
+          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+      )
+
+    image = cv2.flip(image, 1)
+
+    # Convert the image from BGR to RGB as required by the TFLite model.
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
+
+    # Run object detection using the model.
+    detector.detect_async(mp_image, time.time_ns() // 1_000_000)
+
+    # Show the FPS
+    fps_text = 'FPS = {:.1f}'.format(FPS)
+    text_location = (left_margin, row_size)
+    current_frame = image
+    cv2.putText(current_frame, fps_text, text_location, cv2.FONT_HERSHEY_DUPLEX,
+                font_size, text_color, font_thickness, cv2.LINE_AA)
+##
+    if detection_result_list:
+        print(detection_result_list)
+        current_frame = visualize(current_frame, detection_result_list[0])
+        if detection_result_list[0] == "Battery1":
+            battery_counter = 1
+        if detection_result_list[0] == "Battery2":
+            battery_counter = 2
+        if detection_result_list[0] == "Battery3":
+            battery_counter = 3
+        if detection_result_list[0] == "Battery4":
+            battery_counter = 4
+        detection_frame = current_frame
+        detection_result_list.clear()
+
+    if detection_frame is not None:
+        cv2.imshow('object_detection', detection_frame)
+
+    # Stop the program if the ESC key is pressed.
+    if cv2.waitKey(1) == 27:
+      break
+
+  detector.close()
+  cap.release()
+  cv2.destroyAllWindows()
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--model',
+      help='Path of the object detection model.',
+      required=False,
+      default='efficientdet.tflite')
+  parser.add_argument(
+      '--maxResults',
+      help='Max number of detection results.',
+      required=False,
+      default=5)
+  parser.add_argument(
+      '--scoreThreshold',
+      help='The score threshold of detection results.',
+      required=False,
+      type=float,
+      default=0.25)
+  # Finding the camera ID can be very reliant on platform-dependent methods.
+  # One common approach is to use the fact that camera IDs are usually indexed sequentially by the OS, starting from 0.
+  # Here, we use OpenCV and create a VideoCapture object for each potential ID with 'cap = cv2.VideoCapture(i)'.
+  # If 'cap' is None or not 'cap.isOpened()', it indicates the camera ID is not available.
+  parser.add_argument(
+      '--cameraId', help='Id of camera.', required=False, type=int, default=0)
+  parser.add_argument(
+      '--frameWidth',
+      help='Width of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=1280)
+  parser.add_argument(
+      '--frameHeight',
+      help='Height of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=720)
+  args = parser.parse_args()
+
+  run(args.model, int(args.maxResults),
+      args.scoreThreshold, int(args.cameraId), args.frameWidth, args.frameHeight)
+  motorcontrol()
+
+
+if __name__ == '__main__':
+  main()
