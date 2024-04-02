256930daaab8f492ee23f035f54dc7a61b9b8d16
aasocial <143957935+aasocial@users.noreply.github.com>
2024-04-03 00:38:27 +0900

    Update train.py

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ assert tf.__version__.startswith('2')
 tf.get_logger().setLevel('ERROR')
 from absl import logging
 logging.set_verbosity(logging.ERROR)
-
+"""
 train_data = object_detector.DataLoader.from_pascal_voc(
     'Arduino_Image_Training_Set/train',
     'Arduino_Image_Training_Set/train',
@@ -40,7 +40,7 @@ spec = model_spec.get('efficientdet_lite0')
 model = object_detector.create(train_data, model_spec=spec, batch_size=4, train_whole_model=True, epochs=20, validation_data=val_data)
 
 model.evaluate(val_data)
-
+"""
 model.export(export_dir='.', tflite_filename='ArduinoDesign.tflite')
 
 model.evaluate_tflite('ArduinoDesign.tflite', val_data)
