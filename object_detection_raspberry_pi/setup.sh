e501bcbc907258f7fc1a0aa0d94bc1e290f367bd
aasocial <143957935+aasocial@users.noreply.github.com>
2024-05-04 18:50:54 +0900

    Update setup.sh

--- object_detection_raspberry_pi/setup.sh
+++ object_detection_raspberry_pi/setup.sh
@@ -1,9 +1,7 @@
 # Install Python dependencies.
 python3 -m pip install pip --upgrade
-python3 -m pip install -r requirements.txt
 
-wget -q -O efficientdet.tflite -q https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/int8/1/efficientdet_lite0.tflite
-
+sudo apt-get update
 sudo apt-get install python3-rpi.gpio
-sudo pip3 install gpiozero
 pip install RPi.GPIO
+pip install opencv-python numpy
